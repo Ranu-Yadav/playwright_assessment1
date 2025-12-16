b3b60f5b8cf20569309dcaa757e488f9f4d52842
Ranu-Yadav <ranu.yadav@unthinkable.co>
2025-12-16 12:13:58 +0530

    removed some files

--- test_logintest1.py
+++
@@ -1,4 +0,0 @@
-from playwright.sync_api import Page
-
-def test_login1(page:Page):
-    page.goto("https://practicetestautomation.com/practice-test-login/")
--- test_newLogin.py
+++
@@ -1,6 +0,0 @@
-from playwright.sync_api import Page
-
-def test_login(page:Page):
-    page.goto("https://practicetestautomation.com/practice-test-login/")
-    page.fill("#username" , "student")
-    page.fill("#password" , "Password123")
